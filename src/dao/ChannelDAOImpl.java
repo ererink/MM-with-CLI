7804076e820fc7df3e7685e34dbf96f01196b27a
김준서 <adoo244475@gmail.com>
2023-04-05 09:59:15 +0900

    feat: UserAuth 추가

--- src/dao/ChannelDAOImpl.java
+++ src/dao/ChannelDAOImpl.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ChannelDAOImpl implements ChannelDAO{
-    private ChannelDAO instance = new ChannelDAOImpl();
+    private static ChannelDAO instance = new ChannelDAOImpl();
     private ChannelDAOImpl() {
 
     }
