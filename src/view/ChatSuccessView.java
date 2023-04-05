3ca891fc1f48b3bb78360f1784527f9b25e2986c
ererink <yelkim0210@gmail.com>
2023-04-05 19:39:31 +0900

    fix: Chat/ChatView

--- src/view/ChatSuccessView.java
+++ src/view/ChatSuccessView.java
@@ -7,7 +7,9 @@ import java.util.List;
 public class ChatSuccessView {
 
 	public static void selectPrint(List<ChatDTO> list) {
-		System.out.println("======= "+list.size()+" 개가 조회되었습니다 =======");
+		System.out.println();
+		System.out.println("++++++++++++++++++++++++ "+list.size()+" 개가 조회되었습니다 ++++++++++++++++++++++++");
+		System.out.println("    ID       TITLE      CONTENT ");
 		for(ChatDTO chat : list) {
 			System.out.println(chat);
 		}
