a3b88c83cdc503ab8098b933cad92bb90814dbad
김준서 <adoo244475@gmail.com>
2023-04-05 10:46:27 +0900

    feat: UserSession 추가

---
+++ src/session/UserSession.java
@@ -0,0 +1,63 @@
+package session;
+
+import dto.ROLE;
+
+public class UserSession {
+    private String user_id = null;
+    private String user_name = null;
+
+    public void setClass_id(long class_id) {
+        this.class_id = class_id;
+    }
+
+    public void setChannel_id(long channel_id) {
+        this.channel_id = channel_id;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public long getClass_id() {
+        return class_id;
+    }
+
+    public long getChannel_id() {
+        return channel_id;
+    }
+
+    public ROLE getRole() {
+        return role;
+    }
+
+    private long class_id = -1;
+    private long channel_id = -1;
+
+    private ROLE role = null;
+
+    private static UserSession instance = new UserSession();
+    private UserSession() {
+
+    }
+    public static UserSession getInstance() {
+        return instance;
+    }
+
+    public void login(String user_id, String user_name, long class_id, ROLE role) {
+        this.user_id = user_id;
+        this.user_name = user_name;
+        this.class_id = class_id;
+        this.role = role;
+    }
+
+    public void logout() {
+        this.user_id = null;
+        this.user_name = null;
+        this.class_id = -1;
+        this.role = null;
+    }
+}
