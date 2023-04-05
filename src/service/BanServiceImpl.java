a37e40cd16815fb11dd51fa38243f7d029403e69
Ayoung Jo <joahae0302@gmail.com>
2023-04-05 11:32:51 +0900

    test: test

---
+++ src/service/BanServiceImpl.java
@@ -0,0 +1,66 @@
+package service;
+
+import dao.BanDAO;
+import dao.BanDAOImpl;
+import dto.BanDTO;
+
+import java.util.List;
+
+public class BanServiceImpl implements BanService{
+    private static BanService instance = new BanServiceImpl();
+
+    private BanDAO banDAO = BanDAOImpl.getInstance();
+
+    public BanServiceImpl() {
+    }
+
+    public static BanService getInstance() {
+        return instance;
+    }
+
+    @Override
+    public int insertBan(BanDTO banDTO) {
+        int result = banDAO.insertBan(banDTO);
+        if(result == 0 ){
+            throw new RuntimeException("해당 반을 생성할 수 없습니다.");
+        }
+        return result;
+    }
+
+    @Override
+    public List<BanDTO> selectAllBan() {
+        List<BanDTO> banList  = banDAO.selectAllBan();
+
+        if(banList.size() == 0){
+            throw new RuntimeException("반의 정보를 불러올 수 없습니다.");
+        }
+        return banList;
+    }
+
+    @Override
+    public BanDTO selectOneBan(int classNum) {
+        BanDTO banDTO = banDAO.selectOneBan(classNum);
+
+        if(banDTO == null){
+            throw new RuntimeException("해당 반이 존재하지 않습니다.");
+        }
+        return banDTO;
+    }
+    @Override
+    public int updateBan(BanDTO banDTO) {
+        int result = banDAO.updateBan(banDTO);
+        if(result == 0){
+            throw new RuntimeException("반의 정보를 변경할 수 없습니다. 다시 변경해 주세요.");
+        }
+        return result;
+    }
+
+    @Override
+    public int deleteBan(BanDTO banDTO) {
+        int result = banDAO.deleteBan(banDTO);
+        if(result == 0){
+            throw new RuntimeException("반의 정보를 삭제할 수 없습니다. 다시 삭제해 주세요.");
+        }
+        return result;
+    }
+}
