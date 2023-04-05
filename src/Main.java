a93fda921fc4c7186fc5497458824b114c641a60
yeoooo <ruisya330@gmail.com>
2023-04-05 18:52:14 +0900

    feature : login view with main controller

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import controller.MainController;
+
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MainController.controllerService();
     }
+
 }
