b3b6728577b9979e3fa8afd3d8d86ae03063afa9
Ayoung Jo <joahae0302@gmail.com>
2023-04-06 10:58:10 +0900

    Add: BanView 규격 적용 및 전체 뒤로가기 적용

--- src/view/BanView.java
+++ src/view/BanView.java
@@ -1,6 +1,7 @@
 package view;
 
 import controller.BanController;
+import controller.MainController;
 import dto.BanDTO;
 import dto.ROLE;
 import session.UserSession;
@@ -12,23 +13,45 @@ public class BanView {
     static Scanner sc = new Scanner(System.in);
     static UserSession userSession = UserSession.getInstance();
 
+    public static void main(String[] args) {
+        //Test
+        userSession.setRole(ROLE.A);
+        banChoice();
+
+    }
+
+    /**
+     * 반(CLASS) 출력
+     */
     public static void banChoice(){
+        System.out.println("  ______   __         ______    ______   ______  \n" +
+                " /      \\ /  |       /      \\  /      \\ /      \\ \n" +
+                "/$$$$$$  |$$ |      /$$$$$$  |/$$$$$$  /$$$$$$  |\n" +
+                "$$ |  $$/ $$ |      $$ |__$$ |$$ \\__$$/$$ \\__$$/ \n" +
+                "$$ |      $$ |      $$    $$ |$$      \\$$      \\ \n" +
+                "$$ |   __ $$ |      $$$$$$$$ | $$$$$$  |$$$$$$  |\n" +
+                "$$ \\__/  |$$ |_____ $$ |  $$ |/  \\__$$ /  \\__$$ |\n" +
+                "$$    $$/ $$       |$$ |  $$ |$$    $$/$$    $$/ \n" +
+                " $$$$$$/  $$$$$$$$/ $$/   $$/  $$$$$$/  $$$$$$/");
 
             while(true){
                 if(userSession.getRole() == ROLE.A){
-                    System.out.println("\n-------------------------------------------------------------------------------");
-                    System.out.print("[ 0.뒤로 가기    ");
+                    System.out.println();
+                    System.out.print("====================================");
+                    System.out.print("∥ 0 .뒤로 가기   ");
                     System.out.print("1. 반 입장하기   ");
-                    System.out.print("2. 반 관리하기   ]");
-                    System.out.println("\n-------------------------------------------------------------------------------");
-                    System.out.print("기능을 선택하세요 : ");
+                    System.out.print("2. 반 관리하기   ∥");
+                    System.out.println("===================================\n");
+                    System.out.print("선택 ▶ ");
             }
             try{
                 int menu = Integer.parseInt(sc.nextLine());
                 switch (menu){
                     case 0:
-                        userSession.setUser_id("");
-                        return;
+                        if (userSession.getRole() == ROLE.A){
+                            MainView.adminView();
+                            return;
+                        }
                     case 1:
                         selectAllBan();
                         break;
@@ -36,20 +59,22 @@ public class BanView {
                         manageBan();
                         break;
                     default:
-                        System.out.println("잘못된 번호 입니다.");
+                        System.out.println("잘못된 입력입니다. 다시 입력해주세요!");
                 }
             } catch (NumberFormatException e) {
-                System.out.println("숫자를 입력하세요. ");
+                System.out.println("숫자 입력만 가능해요!");
             }
         }
     }
 
+    //메뉴의 각 기능 호출
     /**
      * 1. 반 입장하기
      */
     public static void selectAllBan(){
         BanController.selectAllBan();
-        System.out.print("입장할 반 번호를 입력해주세요.");
+        System.out.println();
+        System.out.println("====================== 입장할 반 번호를 입력해주세요. ======================");
         long class_id = Long.parseLong(sc.nextLine());
         userSession.setClass_id(class_id);
         ChannelView.channelChoice();
@@ -60,15 +85,16 @@ public class BanView {
      * 반 추가/수정/삭제
      */
     public static void manageBan(){
-        BanController.selectAllBan();
         while(true){
-            System.out.println("\n-------------------------------------------------------------------------------");
-            System.out.print("[ 0.뒤로 가기    ");
+            BanController.selectAllBan();
+            System.out.println();
+            System.out.print("============================");
+            System.out.print("∥ 0.뒤로 가기    ");
             System.out.print("1. 반 추가하기   ");
             System.out.print("2. 반 수정하기   ");
-            System.out.print("3. 반 삭제하기   ]");
-            System.out.println("\n-------------------------------------------------------------------------------");
-            System.out.print("기능을 선택하세요 : ");
+            System.out.print("3. 반 삭제하기   ∥");
+            System.out.println("============================\n");
+            System.out.print("선택 ▶ ");
 
             try{
                 int menu = Integer.parseInt(sc.nextLine());
@@ -85,10 +111,10 @@ public class BanView {
                         deleteClass();
                         break;
                     default:
-                        System.out.println("잘못된 번호 입니다.");
+                        System.out.println("잘못된 입력입니다. 다시 입력해주세요!");
                 }
             } catch (NumberFormatException e) {
-                System.out.println("숫자를 입력하세요. ");
+                System.out.println("숫자 입력만 가능해요!");
             }
         }
 
@@ -98,10 +124,22 @@ public class BanView {
      * 2-1. 반 추가
      */
     public static void insertClass(){
-        System.out.print("추가하고 싶은 반 이름을 입력하세요: ");
-        String class_Name = sc.nextLine();
+        System.out.println("====================== 추가하고 싶은 반 이름을 입력하세요 ======================");
+        System.out.println("================= 추가하고 싶은 반이 없다면 X 를 입력해 주세요 =================");
+        System.out.println("※※※※※※※※※※※※※※ 추가하고 싶은 반이 없다면 X 를 입력해 주세요 ※※※※※※※※※※※※※※");
+        System.out.print("반 이름 ▶ ");
 
-        BanController.insertBan(new BanDTO(0,class_Name));
+        try{
+            String class_Name = sc.nextLine();
+            switch (class_Name){
+                case "X":
+                    return;
+                default:
+                    BanController.insertBan(new BanDTO(0,class_Name));
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("문자 입력만 가능해요!");
+        }
     }
 
     /**
@@ -109,12 +147,25 @@ public class BanView {
      */
     public static void updateClass(){
         BanController.selectAllBan();
-        System.out.print("수정하고 싶은 반 번호를 입력해주세요.");
-        long class_id = Long.parseLong(sc.nextLine());
-        System.out.print("수정하고 싶은 반 이름을 정확하게 입력해주세요.");
-        String class_Name = sc.nextLine();
+        System.out.println("====================== 수정하고 싶은 반 번호를 입력해주세요 ======================");
+        System.out.println("※※※※※※※※※※※※※※ 수정하고 싶은 반이 없다면 0을 입력해 주세요 ※※※※※※※※※※※※※※");
+        System.out.print("반 번호 ▶ ");
 
-        BanController.updateBan(new BanDTO(class_id, class_Name));
+        try{
+            long class_id = Long.parseLong(sc.nextLine());
+            switch ((int) class_id){
+                case 0:
+                    return;
+                default:
+                    System.out.println("====================== 수정하고 싶은 반 이름을 입력해주세요 ======================");
+                    System.out.print("반 이름 ▶ ");
+                    String class_Name = sc.nextLine();
+                    BanController.updateBan(new BanDTO(class_id, class_Name));
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("숫자 입력만 가능해요!");
+        }
+
     }
 
     /**
@@ -122,9 +173,22 @@ public class BanView {
      */
     public static void deleteClass(){
         BanController.selectAllBan();
-        System.out.print("삭제하고 싶은 반 번호를 입력해주세요.");
-        int class_id = Integer.parseInt(sc.nextLine());
-        BanController.deleteBan(class_id);
+        System.out.println("====================== 삭제하고 싶은 반 번호를 입력해주세요 ======================");
+        System.out.println("※※※※※※※※※※※※※※ 삭제하고 싶은 반 번호가 없다면 0을 입력해 주세요 ※※※※※※※※※※※※※※");
+
+        System.out.print("반 번호 ▶ ");
+
+        try{
+            int class_id = Integer.parseInt(sc.nextLine());
+            switch (class_id){
+                case 0:
+                    return;
+                default:
+                    BanController.deleteBan(class_id);
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("숫자 입력만 가능해요!");
+        }
     }
 
 }
