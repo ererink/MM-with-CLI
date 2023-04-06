ff20d70f290cc3377d59f5ece80daf1223d516a9
ererink <yelkim0210@gmail.com>
2023-04-06 09:59:04 +0900

    fix: ChannelView 규격 적용

--- src/view/ChannelView.java
+++ src/view/ChannelView.java
@@ -16,6 +16,12 @@ public class ChannelView {
     static Scanner sc = new Scanner(System.in);
     private static ChannelService channelService = ChannelServiceImpl.getInstance();
     static UserSession userSession = UserSession.getInstance();
+
+    public static void main(String[] args) {
+        userSession.setUser_id("adoo24");
+
+        channelChoice();
+    }
     public static void channelChoice() {
         System.out.println("  ______   __                                                __ \n" +
                 " /      \\ /  |                                              /  |\n" +
@@ -27,14 +33,18 @@ public class ChannelView {
                 "$$    $$/ $$ |  $$ |$$    $$ |$$ |  $$ |$$ |  $$ |$$       |$$ |\n" +
                 " $$$$$$/  $$/   $$/  $$$$$$$/ $$/   $$/ $$/   $$/  $$$$$$$/ $$/ ");
         while (true) {
-            System.out.println("\n-------------------------------------------------------------------------------");
-            System.out.print("[ 0. 뒤로 가기    ");
-            System.out.print("1. 채널 입장   ");
-            System.out.print("2. 채널 추가   ");
+            System.out.println();
+            System.out.print("===================================");
+            System.out.print(" CHANNEL [ "+userSession.getChannel_name()+" ] ");
+            System.out.print("===================================\n");
+            System.out.print("∥  0. 뒤로 가기   ");
+            System.out.print("1. 채널 입장하기   ");
+            System.out.print("2. 채널 추가하기   ");
             System.out.print("3. 채널 업데이트   ");
-            System.out.print("4. 채널 삭제   ]");
-            System.out.println("\n------------------------------------------------------------------------------");
-            System.out.print("기능을 선택하세요: ");
+            System.out.print("4. 채널 삭제하기  ∥");
+            System.out.println("\n========================================================================================");
+            System.out.print("선택 ▶ ");
+
             try {
                 int menu = Integer.parseInt(sc.nextLine());
                 switch (menu) {
@@ -54,17 +64,21 @@ public class ChannelView {
                         deleteChannel();
                         break;
                     default:
-                        System.out.println("잘못된 번호 입니다.");
+                        System.out.println("잘못된 입력입니다. 다시 입력해주세요!");
                 }
             } catch (NumberFormatException e) {
-                System.out.println("숫자를 입력하세요. ");
+                System.out.println("숫자 입력만 가능해요!");
             }
         }
     }
 
     public static void enterChannel() {
         ChannelController.channelSelect();
-        System.out.print("입장할 채널 번호를 입력해주세요: ");
+        System.out.println();
+        System.out.println("====================== 입장할 채널 번호를 입력해주세요 ======================");
+        
+        // 입력값
+        System.out.print("채널 번호 ▶ ");
         int channel_id = Integer.parseInt(sc.nextLine());
         userSession.setChannel_id(channel_id);
         userSession.setChannel_name(channelService.selectOneChannel(channel_id).getChannel_name());
@@ -74,16 +88,19 @@ public class ChannelView {
 
 
     public static void inputInsertChannel() {
-        System.out.print("공개채널로 만드시려면 1, 아니라면 0을 입력해주세요: ");
+        System.out.println();
+        System.out.println("==================== √ 공개채널로 만드시려면 1, 아니라면 0을 입력해주세요 √ ====================");
+        System.out.print("입력 ▶ ");
         int isOpen = Integer.parseInt(sc.nextLine());
-        System.out.print("채널 제목을 입력하세요: ");
+        System.out.print("채널 제목 ▶ ");
         String channelName = sc.nextLine();
         if (isOpen == 0) {
             //반에 있는 유저들 리스트 출력
             Set<String> set = new HashSet<>();
             set.add(userSession.getUser_id());
             while (true) {
-                System.out.print("추가할 유저를 입력하세요(그만하려면 q를 입력): ");
+                System.out.print("추가할 유저의 ID (그만하기 q 입력) ▶ ");
+
                 String temp = sc.nextLine();
                 if (temp.equals("q")) {
                     break;
@@ -100,16 +117,21 @@ public class ChannelView {
 
     public static void inputUpdateChannel() {
         ChannelController.channelSelect();
-        System.out.print("업데이트할 채널 번호를 입력해주세요: ");
+        System.out.println();
+        System.out.println("========================== 수정할 채널번호를 입력해주세요 ==========================");
+        System.out.print("채널 번호 ▶ ");
+
         long channel_id = Long.parseLong(sc.nextLine());
-        System.out.print("채널 제목을 입력하세요: ");
+        System.out.print("채널 제목 ▶ ");
         String channelName = sc.nextLine();
         ChannelController.channelUpdate(new ChannelDTO(channel_id,channelName,0,0));
     }
 
     public static void deleteChannel() {
         ChannelController.channelSelect();
-        System.out.print("삭제할 채널 번호를 입력해주세요: ");
+        System.out.println();
+        System.out.println("========================== 삭제할 채널 번호를 입력해주세요 ==========================");
+        System.out.print("채널 번호 ▶ ");
         int channel_id = Integer.parseInt(sc.nextLine());
         ChannelController.channelDelete(channel_id);
     }
